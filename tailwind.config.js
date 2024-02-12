7ef6b0cd53723e20d906f8be28fd71a35c70f761
Bagus Arya <aryabagus453@gmail.com>
2024-02-12 14:25:19 +0800

    asdasd

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,15 @@ module.exports = {
       colors : {
         "biru" : '#5160E2',
         "abu"  : '#191825'
+      },
+      dropShadow : {
+        "button" : ['0px 0px 0px 0px rgba(0, 0, 0, 0.10)', 
+        '0px 5px 11px 0px rgba(0, 0, 0, 0.10)',
+        '0px 20px 20px 0px rgba(0, 0, 0, 0.09)',
+        '0px 45px 27px 0px rgba(0, 0, 0, 0.05)', 
+        '0px 81px 32px 0px rgba(0, 0, 0, 0.01)', 
+        '0px 126px 35px 0px rgba(0, 0, 0, 0.00)'
+        ]
       }
     },
   },
